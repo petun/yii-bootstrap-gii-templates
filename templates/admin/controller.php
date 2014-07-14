e9627328626727c409c9793af21b737706d33598
Nikita Ignatenkov <Kitingg@users.noreply.github.com>
2014-07-14 12:03:56 +0400

    Update controller.php
    Исправил redirect после событий создания и обновления

--- templates/admin/controller.php
+++ templates/admin/controller.php
@@ -39,7 +39,7 @@ class <?php echo $this->controllerClass; ?> extends <?php echo $this->baseContro
 		if (isset($_POST['<?php echo $this->modelClass; ?>'])) {
 			$model->attributes=$_POST['<?php echo $this->modelClass; ?>'];
 			if ($model->save()) {
-				$this->redirect(array('view','id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>));
+				$this->redirect('index');
 			}
 		}
 
@@ -63,7 +63,7 @@ class <?php echo $this->controllerClass; ?> extends <?php echo $this->baseContro
 		if (isset($_POST['<?php echo $this->modelClass; ?>'])) {
 			$model->attributes=$_POST['<?php echo $this->modelClass; ?>'];
 			if ($model->save()) {
-				$this->redirect(array('view','id'=>$model-><?php echo $this->tableSchema->primaryKey; ?>));
+				$this->redirect('index');
 			}
 		}
 
